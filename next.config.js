0f362962cd3b0a284868fb6a1619d9ba610f06b5
João Pedro Meirelles Ventania <ventania91@gmail.com>
2025-04-28 18:28:29 -0300

    fix: update next.config.js for production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: 'standalone',
+  images: {
+    domains: ['github.com'],
+  },
 }
 
 module.exports = nextConfig 
